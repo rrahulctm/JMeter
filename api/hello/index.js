1390f607d61b67a6d74d2a92fc3d36086d10f3ba
rrahulctm <115645454+rrahulctm@users.noreply.github.com>
2025-05-29 12:17:28 +0530

    Delete api/hello directory

--- api/hello/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  context.log("HTTP trigger function processed a request.");
-  context.res = {
-    status: 200,
-    body: "Hello from Azure Function!",
-  };
-};
